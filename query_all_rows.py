6713768ce59ba7ba1a20952110c1f41399c41edd
biyyapu <73270239+biyyapu@users.noreply.github.com>
2021-09-19 14:28:26 +0530

    Create query_all_rows.py

---
+++ query_all_rows.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+#conect to sqlite3
+conn=sqlite3.connect("movies.db")
+
+#creating a cursor
+cur=conn.cursor()
+
+# inserting multiple movies details
+moviesList= [('AA','Nithin','samantha','Trivikram',2018),
+             ('murari','Mahesh Babu','sonali bindre','krishna Vamsi',2001),
+             ('Mirchi','prabhas','Anushka Shetty','Koratala Siva',2015)]
+
+cur.executemany("insert into Movies values(?,?,?,?,?)",moviesList)
+
+#committing the changes and closing
+conn.commit()
+conn.close()         
